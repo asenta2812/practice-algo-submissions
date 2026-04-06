517b3bf6ccb5d8c589d9d8a8e78aa7232e6533d7
Trong Nghia <52541823+asenta2812@users.noreply.github.com>
2026-04-06 19:56:00 +0700

    Add: valid-word-square - submission-5

---
+++ Data Structures & Algorithms/valid-word-square/submission-5.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {string[]} words
+     * @return {boolean}
+     */
+    validWordSquare(words) {
+        for (let columnWord = 0; columnWord < words.length; columnWord++) {
+            for (let rowWord = 0; rowWord < words[columnWord].length; rowWord++) {
+                if (
+                    rowWord >= words.length || 
+                    columnWord >= words[rowWord].length ||
+                    words[columnWord][rowWord] !== words[rowWord][columnWord]) {
+                    return false
+                }
+            }
+        }
+
+        return true
+    }
+}
