76bdbe97873983d621a8ed9526a1a31ba5b28a75
Trong Nghia <52541823+asenta2812@users.noreply.github.com>
2026-04-09 16:44:49 +0700

    Add: longest-consecutive-sequence - submission-3

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-3.js
@@ -0,0 +1,35 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    longestConsecutive(nums) {
+        const n = nums.length
+        if (!n) {
+            return 0
+        }
+
+        const m = {}
+        let res = 0
+        for (let num of nums) {
+            if (m[num]) {
+                continue
+            }
+
+            /*
+            *Hãy tưởng tượng mỗi con số là một viên gạch. Khi bạn đặt viên gạch num xuống:      
+                Nó có thể nối một dãy bên trái (kết thúc tại num - 1).
+                Nó có thể nối một dãy bên phải (bắt đầu tại num + 1).
+            */
+            const left = m[num-1] || 0
+            const right = m[num+1] || 0
+            const newLen = left + right + 1
+            res = Math.max(newLen, res)
+            m[num] = newLen
+            m[num-left] = newLen
+            m[num+right] = newLen
+        }
+
+        return res
+    }
+}
